6a82e540250ea6b3e117f025874bbe90c7f1b176
gitsammy <sinsixie@gmail.com>
2015-06-20 03:29:28 -0700

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,20 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  # Configure rails to connect with e-mail server.
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DOMAIN_NAME"],
+    authentication: "plain",
+    enable_startttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+}
+
+  # Send e-mail in development mode
+  config.action_mailer.perform_deliveries = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
